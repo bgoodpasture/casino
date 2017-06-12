596aa8f246c0aa2107a1dc9383a433e036616abe
Bradford Goodpasture <bgoodpasture@gmail.com>
2017-06-12 18:31:01 -0400

    added equipment page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+
+  get '/', to: 'home#index'
+  get 'equipment', to: 'home#equipment'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
